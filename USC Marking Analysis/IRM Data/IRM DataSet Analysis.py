6c7dac1bd0f21c718b21658426e3c08b617a3160
samsmitt98 <samsmith.hf@gmail.com>
2024-11-15 10:51:33 +1100

    Lines of code added

--- USC Marking Analysis/IRM Data/IRM DataSet Analysis.py
+++ USC Marking Analysis/IRM Data/IRM DataSet Analysis.py
@@ -1,11 +1,27 @@
 import pandas as pd
 
 
-df = pd.read_csv(r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\SW-Tax\USC Marking Analysis\IRM Data\IRM_AssessmentComponent_20240926.csv")
+import pandas as pd
 
+# Specify a different encoding
+df = pd.read_csv(r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\SW-Tax\USC Marking Analysis\IRM Data\IRM_AssessmentComponent_20240926.csv", encoding='ISO-8859-1')
 
 # DATA QUALITY
 
 #1. Check for missing values
 
+print("Number of missing values by column: ")
 print(df.isnull().sum())
+
+
+#2. Check for duplicates 
+
+print("Number of duplicated rows: ")
+print(df.duplicated().sum())
+
+
+# DATA STRUCTURE AND TYPES
+
+#check the shape of the data
+print("Data set shape No. Rows : No. Columns ")
+print(df.shape)
