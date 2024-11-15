c174e98341fa09584f5e1643b201852c109fdcca
samsmitt98 <samsmith.hf@gmail.com>
2024-11-15 11:56:05 +1100

    Added Histogram

--- USC Marking Analysis/IRM Data/IRM DataSet Analysis.py
+++ USC Marking Analysis/IRM Data/IRM DataSet Analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-
-import pandas as pd
 
 # Specify a different encoding
 df = pd.read_csv(r"C:\Users\smits\OneDrive - SW Accountants & Advisors Pty Ltd\Desktop\SW-Tax\USC Marking Analysis\IRM Data\IRM_AssessmentComponent_20240926.csv", encoding='ISO-8859-1')
@@ -48,3 +48,75 @@ def find_extremes(df, column):
 #Check by column for any extreme values
 min_val, max_val = find_extremes(df, 'Weight')
 print(f"Minimum: {min_val}, Maximum: {max_val}")
+
+
+#Find the 5 smallest values
+
+print("5 smallest values within the column")
+smallest_values = df['Weight'].nsmallest(5)
+print("Smallest values:\n", smallest_values)
+
+
+# Find the 5 largest values
+print("5 largest values within the column")
+largest_values = df["Weight"].nlargest(5)
+print("largest values\n", largest_values)
+
+
+#find the 5 smallest unqiue values
+print("5 smallest unique values within the column")
+smallest_Unique_values = df['Weight'].sort_values().unique()[:5]
+print("Smallest Unique values:\n", smallest_Unique_values)
+
+
+#find the 5 largest unqiue values 
+print("5 largest unique values within the column")
+largest_Unique_values = df['Weight'].sort_values(ascending=False).unique()[:5]
+print("Largest Unique values:\n",largest_Unique_values)
+
+
+#DATA DISTRIBUTIONS & SUMMARY STATISTICS
+
+
+def calucate_statistics(df, column):
+    '''
+    Returns the mean, median, and mode for a specified column in the DataFrame. 
+
+    Parameters:
+        df (pd.DataFrame) : The DataFrame containing the data. 
+        column (str): The column name to caculate statistic for. 
+
+    Returns:
+        dict: A dictionary containing the mean, median, and mode of the column.
+
+    '''
+    mean_value = df[column].mean()
+    median_value = df[column].median()
+    mode_value = df[column].mode().iloc[0] if not df[column].mode().empty else None
+
+    return {
+        'mean' : mean_value,
+        'median' : median_value,
+        'mode' : mode_value
+    }
+
+print(calucate_statistics(df, 'Weight'))
+
+def plot_histogram_seaborn(df, column, bins=10):
+    '''
+    Creates a histogram for a specified column using seaborn. 
+
+    Parameters:
+        df (pd.DataFrame): The DataFrame containing the data. 
+        column (str): The column name to create a hisogram for. 
+        bins (int): The number of bins in the histogram (default is 10).
+    '''
+
+    plt.figure(figsize=(10, 6))
+    sns.histplot(df[column].dropna(), bins=bins, kde=True, edgecolor='black')
+    plt.xlabel(column)
+    plt.ylabel('Frequency')
+    plt.xlabel(f'Hisogram of {column}')
+    plt.show()
+
+plot_histogram_seaborn(df, 'Weight', bins=10)
